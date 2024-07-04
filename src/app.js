f07194547719d5c24ddd2c9871dd7968ec1c894c
José Ricardo <josericardosaraiva7@gmail.com>
2024-07-04 14:57:19 -0300

    config: typescript

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const fastify = require("fastify")
-
-const app = fastify({logger: true})
-
-app.get("/hello", (request, reply) => {
-    reply.send({ message: "hello, world!" })
-})
-
-module.exports = app
